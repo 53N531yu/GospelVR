d131fc3a538b651a4bf04506c9b4a91a627415a5
53N531yu <oscarloltiong@gmail.com>
2024-11-01 22:06:03 -0400

    Player is now able to sit on the bench and open the journal

--- Gospel/Assets/Scripts/Bench.cs
+++ Gospel/Assets/Scripts/Bench.cs
@@ -7,15 +7,21 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using UnityEngine.Windows;
+using UnityEngine.InputSystem;
 
 public class Bench : MonoBehaviour
 {
     public GameObject journal;
+    public GameObject journalNavigator;
+    public GameObject journalButton;
+    public GameObject pressToSit;
     public SaveManager save;
     public VerseManager verse;
+    public InputActionProperty collectVerseButton;
+    public GameObject move;
     public bool canSit;
     public bool isSitting;
-    public bool isViewing = false;
+    public bool isReading = false;
     
     void Start()
     {
@@ -23,6 +29,12 @@ public class Bench : MonoBehaviour
         {
             journal = GameObject.Find("Journal");
         }
+
+        if (journalButton == null)
+        {
+            journalButton = GameObject.Find("Open Journal");
+        }
+
         if (save == null)
         {
             save = GameObject.Find("Save Manager").GetComponent<SaveManager>();
@@ -32,22 +44,25 @@ public class Bench : MonoBehaviour
         {
             verse = GameObject.Find("Verse Manager").GetComponent<VerseManager>();
         }
+
+        if (pressToSit == null)
+        {
+            pressToSit = GameObject.Find("Press To Sit");
+        }
+
+        if (move == null)
+        {
+            move = GameObject.Find("Move");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canSit) BenchSit();
+        if (canSit && collectVerseButton.action.WasPressedThisFrame()) BenchSit();
 
-        if (isSitting) 
-        {
-            DisplayBenchUI();
-        }
-
-        else if (!isSitting)
-        {
-            HideBenchUI();
-        }
+        if (canSit && !isSitting) pressToSit.SetActive(true);
+        else if (!canSit || isSitting || isReading) pressToSit.SetActive(false);
     }
 
     public void DisplayBenchUI()
@@ -62,23 +77,37 @@ public class Bench : MonoBehaviour
 
     public void OpenJournal()
     {
-        
+        isReading = true;
+        pressToSit.SetActive(false);
+        journal.SetActive(true);
+        journalNavigator.SetActive(true);
+        journalButton.SetActive(false);
     }
 
     public void CloseJournal()
     {
-        
+        isReading = false;
+        journal.SetActive(false);
+        journalNavigator.SetActive(false);
+        BenchSit();
     }
 
     public void BenchSit()
     {
         isSitting = true;
+        journalButton.SetActive(true);
+        move.SetActive(false);
         save.Save();
     }
 
     public void ExitBench()
     {
         isSitting = false;
+        isReading = false;
+        journal.SetActive(false);
+        journalNavigator.SetActive(false);
+        journalButton.SetActive(false);
+        move.SetActive(true);
     }
 
     void OnTriggerStay(Collider col)
